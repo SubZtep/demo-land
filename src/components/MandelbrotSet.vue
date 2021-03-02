c91958c386d1be216229c228b7c191381fde4d8d
Andras Serfozo <subztep@gmail.com>
2021-03-02 22:14:56 +0000

    Add Zoom

--- src/components/MandelbrotSet.vue
+++ src/components/MandelbrotSet.vue
@@ -1,13 +1,15 @@
 <template lang="pug">
 .grid(:style="cssVars")
   div(
-    v-for="idx in divs"
-    :key="idx"
-    :style="`background-color: ${map.get(`${idx % width}-${~~(idx / width)}`) ?? 'transparent'}`")
+    v-for="idx in divs",
+    :key="idx",
+    :style="`background-color: ${map.get(`${idx % width}-${~~(idx / width)}`) ?? 'transparent'}`",
+    @click="zoom(idx % width, ~~(idx / width))"
+  )
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from "vue"
+import { defineComponent, ref, onUnmounted } from "vue"
 import MandelbrotWorker from "../workers/mandelbrot?worker"
 
 export default defineComponent({
@@ -25,12 +27,16 @@ export default defineComponent({
       type: Number,
       default: 80,
     },
+    zoomFactor: {
+      type: Number,
+      default: 0.1,
+    },
   },
-  setup: ({ width, height, maxIteration }) => {
+  setup: ({ width, height, maxIteration, zoomFactor }) => {
     const map = ref<MandelbrotSetMap>(new Map<string, string>())
 
-    const realSet = { start: -2, end: 1 }
-    const imaginarySet = { start: -1, end: 1 }
+    let realSet = { start: -2, end: 1 }
+    let imaginarySet = { start: -1, end: 1 }
     const colors = new Array(16)
       .fill(0)
       .map((_, i) => (i === 0 ? "#000" : `#${(((1 << 24) * Math.random()) | 0).toString(16)}`))
@@ -41,6 +47,11 @@ export default defineComponent({
       map.value = data
     }
 
+    onUnmounted(() => worker.terminate())
+
+    const getRelativePoint = (pos: number, length: number, set: NumberSet) =>
+      set.start + (pos / length) * (set.end - set.start)
+
     return {
       divs: Array.from({ length: width * height }, (_, i) => i),
       map,
@@ -49,6 +60,21 @@ export default defineComponent({
       cssVars: {
         "--width": width,
         "--height": height,
+      },
+      zoom: (x: number, y: number) => {
+        const zfw = width * zoomFactor
+        const zfh = height * zoomFactor
+
+        realSet = {
+          start: getRelativePoint(x - zfw, width, realSet),
+          end: getRelativePoint(x + zfw, width, realSet),
+        }
+        imaginarySet = {
+          start: getRelativePoint(y - zfh, height, imaginarySet),
+          end: getRelativePoint(y + zfh, height, imaginarySet),
+        }
+
+        worker.postMessage({ width, height, maxIteration, realSet, imaginarySet, colors })
       },
     }
   },
@@ -65,6 +91,7 @@ export default defineComponent({
 }
 
 .grid > div {
+  cursor: pointer;
   width: 8px;
   height: 8px;
 }
