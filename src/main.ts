fbc5e3ef331e613656496b29ba0c254f87bacc39
Andras Serfozo <subztep@gmail.com>
2021-05-12 23:43:35 +0100

    params

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,16 @@ import App from "./App.vue"
 import "virtual:windi.css"
 import "virtual:windi-devtools"
 import "./styles/main.css"
+import { createWebHistory } from "vue-router"
 // import "./styles/global.css"
 // import "./styles/animations.css"
 
-const routes = setupLayouts(generatedRoutes)
+const routerOptions = {
+  routes: setupLayouts(generatedRoutes),
+  history: createWebHistory()
+}
 
-export const createApp = ViteSSG(App, { routes }, ctx => {
+export const createApp = ViteSSG(App, routerOptions, ctx => {
   Object.values(import.meta.globEager("./modules/*.ts")).map(i => i.install?.(ctx))
 
   ctx.router.beforeEach((to, from) => {
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,87 +1,50 @@
 <template lang="pug">
-transition(name="bounce")
-  Settings(
-    v-if="showSettings"
-    v-model:width="width"
-    v-model:height="height"
-    v-model:maxIteration="maxIteration"
-    v-model:paletteSize="paletteSize"
-    v-model:zoomFactor="zoomFactor")
-
 MandelbrotSet(
   :width="width"
   :height="height"
   :maxIteration="maxIteration"
   :paletteSize="paletteSize"
   :zoomFactor="zoomFactor"
-  :key="`${width}-${height}-${maxIteration}`"
-  @toggleSettings="showSettings = !showSettings")
+  :key="`${width}-${height}-${maxIteration}`")
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue"
-// import Settings from "../components/Settings.vue"
-// import MandelbrotSet from "../components/MandelbrotSet.vue"
-import { useWindowSize, useMouse, useCssVar, throttledWatch } from "@vueuse/core"
+import { defineComponent, ref } from "vue"
 
 export default defineComponent({
-  name: "App",
-  // components: {
-  //   MandelbrotSet,
-  //   Settings,
-  // },
-  setup() {
-    const { width: w, height: h } = useWindowSize()
-    const { x, y } = useMouse()
-    const px = useCssVar("--px")
-    const py = useCssVar("--py")
-
-    throttledWatch(
-      [w, h, x, y],
-      () => {
-        px.value = String(~~((x.value / w.value) * 100))
-        py.value = String(~~((y.value / h.value) * 100))
-      },
-      {
-        throttle: 16,
-      }
-    )
-  },
   data() {
     return {
-      showSettings: false,
-      width: 15,
-      height: 10,
-      maxIteration: 30,
-      paletteSize: 100,
-      zoomFactor: 0.1,
+      width: ref(15),
+      height: ref(10),
+      maxIteration: ref(30),
+      paletteSize: ref(100),
+      zoomFactor: ref(0.1),
     }
-  },
+  }
 })
+</script>
+
+<script setup lang="ts">
+import { useWindowSize, useMouse, useCssVar, throttledWatch } from "@vueuse/core"
+
+const { width: w, height: h } = useWindowSize()
+const { x, y } = useMouse()
+const px = useCssVar("--px")
+const py = useCssVar("--py")
+
+throttledWatch(
+  [w, h, x, y],
+  () => {
+    px.value = String(~~((x.value / w.value) * 100))
+    py.value = String(~~((y.value / h.value) * 100))
+  },
+  {
+    throttle: 16,
+  }
+)
 </script>
 
 <route lang="yaml">
 meta:
   layout: blank
-  ccc: sunny
 </route>
-
-<style scoped>
-.bounce-enter-active {
-  animation: bounce-in 0.5s;
-}
-.bounce-leave-active {
-  animation: bounce-in 0.5s reverse;
-}
-@keyframes bounce-in {
-  0% {
-    transform: scale(0);
-  }
-  50% {
-    transform: scale(1.25);
-  }
-  100% {
-    transform: scale(1);
-  }
-}
-</style>
