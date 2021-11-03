cac950c6057246deccc893b58eb9879fac12b6de
Andras Serfozo <subztep@gmail.com>
2021-11-03 03:36:38 +0000

    MONOREPO (but bit wip broken:beetle:)

---
+++ packages/fa-icons/src/index.ts
@@ -0,0 +1,32 @@
+// import type { IconPack } from "@fortawesome/fontawesome-common-types"
+import { library } from "@fortawesome/fontawesome-svg-core"
+// import { fad } from "@fortawesome/pro-duotone-svg-icons"
+// import { fal } from "@fortawesome/pro-light-svg-icons"
+// import { far } from "@fortawesome/pro-regular-svg-icons"
+// import { fas } from "@fortawesome/pro-solid-svg-icons"
+// import { fat } from "@fortawesome/pro-thin-svg-icons"
+
+// library.add(fad as IconPack)
+// library.add(fal as IconPack)
+// library.add(far as IconPack)
+// library.add(fas as IconPack)
+// library.add(fat as IconPack)
+
+import { faCodepen } from "@fortawesome/free-brands-svg-icons/faCodepen"
+import { faGithub } from "@fortawesome/free-brands-svg-icons/faGithub"
+import { faInstagram } from "@fortawesome/free-brands-svg-icons/faInstagram"
+import { faLastfm } from "@fortawesome/free-brands-svg-icons/faLastfm"
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons/faLinkedin"
+import { faPinterest } from "@fortawesome/free-brands-svg-icons/faPinterest"
+import { faSteam } from "@fortawesome/free-brands-svg-icons/faSteam"
+import { faTwitter } from "@fortawesome/free-brands-svg-icons/faTwitter"
+import { faYoutube } from "@fortawesome/free-brands-svg-icons/faYoutube"
+library.add(faCodepen, faGithub, faInstagram, faLastfm, faLinkedin, faPinterest, faSteam, faTwitter, faYoutube)
+
+import { faSliders } from "@fortawesome/pro-regular-svg-icons/faSliders"
+library.add(faSliders)
+
+import { faTrash } from "@fortawesome/pro-regular-svg-icons/faTrash"
+library.add(faTrash)
+
+export { library }
---
+++ web/vite.config.ts
@@ -0,0 +1,83 @@
+import path from "path"
+import { defineConfig } from "vite"
+import vue from "@vitejs/plugin-vue"
+import Prism from "markdown-it-prism"
+import Pages from "vite-plugin-pages"
+import Markdown from "vite-plugin-md"
+import WindiCSS from "vite-plugin-windicss"
+import Layouts from "vite-plugin-vue-layouts"
+import Components from "unplugin-vue-components/vite"
+import AutoImport from "unplugin-auto-import/vite"
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      "~/": `${path.resolve(__dirname, "src")}/`,
+    },
+  },
+
+  plugins: [
+    vue({
+      include: [/\.vue$/, /\.md$/],
+    }),
+
+    Pages({
+      extensions: ["vue", "md"],
+    }),
+
+    Layouts({
+      layoutsDir: "src/layouts",
+    }),
+
+    Markdown({
+      wrapperClasses: "prose prose-sm m-auto text-left",
+      headEnabled: true,
+      markdownItSetup(md) {
+        md.use(Prism)
+      },
+    }),
+
+    Components({
+      dirs: ["src/components", "src/layouts/src/pages"],
+      extensions: ["vue", "md", "ts"],
+      dts: "src/types/components.d.ts",
+      deep: true,
+      // include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
+    }),
+
+    AutoImport({
+      include: [/\.[tj]s$/, /\.vue\??/],
+      imports: [
+        "vue",
+        "@vueuse/core",
+        { "@vueuse/core": ["get", "set", "and", "not", "invoke"] },
+      ],
+      dts: "src/types/auto-imports.d.ts",
+    }),
+
+    WindiCSS({
+      // safelist: "prose prose-sm m-auto text-left",
+    }),
+
+    // VueI18n({
+    //   include: [path.resolve(__dirname, "locales/**")],
+    // }),
+  ],
+
+  // ssgOptions: {
+  //   mode: "production",
+  //   script: "async",
+  //   formatting: "minify",
+  //   dirStyle: "nested",
+  // },
+
+  // optimizeDeps: {
+  //   include: ["vue", "vue-router", "@vueuse/core"],
+  //   exclude: ["vue-demi"],
+  // },
+
+  // server: {
+  //   https: true,
+  //   host: "192.168.0.10"
+  // }
+})
