fbaeb40e7d4789fff87713c55c7d0af7879e99f3
Andras Serfozo <subztep@gmail.com>
2021-11-07 05:42:02 +0000

    test build

--- dist/assets/home.725108d2.js
+++
@@ -1,1 +0,0 @@
-import{e as o,v as r,o as t}from"./vendor.d06f69ff.js";import{_}from"./app.4783da1d.js";function n(s,c){const e=o("router-view");return t(),r(e)}const a={};var m=_(a,[["render",n]]);export{m as default};
--- dist/assets/timeline.d42c2e10.js
+++
@@ -1,1 +0,0 @@
-import{e as r,c as l,f as a,n as t,o as c}from"./vendor.d06f69ff.js";import{_ as n}from"./app.4783da1d.js";function _(s,m){const o=r("router-view");return c(),l("div",{class:t(["container",s.$style.scrollable])},[a(o)],2)}const d="_scrollable_1uqte_6";var i={scrollable:d};const f={},e={};e.$style=i;var v=n(f,[["render",_],["__cssModules",e]]);export{v as default};
