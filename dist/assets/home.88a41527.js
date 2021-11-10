af46712d470788be14ea628e79dcbd5f063386d4
Andras Serfozo <subztep@gmail.com>
2021-11-10 17:24:59 +0000

    txt

---
+++ dist/assets/home.88a41527.js
@@ -0,0 +1,1 @@
+import{e as o,v as r,o as t}from"./vendor.5ddc34c4.js";import{_}from"./app.a520dfeb.js";function n(c,s){const e=o("router-view");return t(),r(e)}const a={};var v=_(a,[["render",n]]);export{v as default};
